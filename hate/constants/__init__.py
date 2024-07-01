48ce4387a2719c4fd3dc38e579cb772ec7ea1dcf
PratikKujur <pratikkujur121@gmail.com>
2024-07-01 19:23:57 +0530

    model evaluation and model pusher added

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -37,7 +37,7 @@ X_TEST_FILE_NAME = 'x_test.csv'
 Y_TEST_FILE_NAME = 'y_test.csv'
 X_TRAIN_FILE_NAME = 'x_train.csv'
 RANDOM_STATE = 42
-EPOCH = 2
+EPOCH = 1
 BATCH_SIZE = 128
 VALIDATION_SPLIT = 0.2
 
@@ -47,3 +47,11 @@ MAX_LEN = 300
 LOSS = 'binary_crossentropy'
 METRICS = ['accuracy']
 ACTIVATION = 'sigmoid'
+
+# Model  Evaluation constants
+MODEL_EVALUATION_ARTIFACTS_DIR = 'ModelEvaluationArtifacts'
+BEST_MODEL_DIR = "best_Model"
+MODEL_EVALUATION_FILE_NAME = 'loss.csv'
+MODEL_NAME = 'model.h5'
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
